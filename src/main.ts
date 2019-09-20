4dbe8f3c232f4be1ffaebd698a3a736b9c350363
Simon Warta <simon@warta.it>
2019-09-20 16:41:40 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,88 @@
+const fs = require('fs');
+import {Decimal} from "@iov/encoding"
+import {add, relative} from "./math"
+
+type Amount = string | { whole?: number, fractional?: number, ticker: string }
+
+interface Wallet {
+  readonly address: string,
+  readonly coins: readonly Amount[],
+}
+
+interface Escrow {
+  readonly arbiter: string,
+  readonly destination: string,
+  readonly source: string,
+  readonly timeout: string,
+  readonly amount: readonly Amount[],
+}
+
+interface Genesis {
+  readonly app_state: {
+    readonly cash: readonly Wallet[],
+    readonly escrow: readonly Escrow[],
+  }
+}
+
+async function getFile(): Promise<Genesis> {
+  return new Promise((resolve, reject) => {
+    fs.readFile('genesis.json', function (err: any, data: any) {
+      if (err) reject(err);
+      else resolve(JSON.parse(data.toString()));
+    });
+  })
+}
+
+function parseIov(input: Amount): Decimal {
+  if (typeof input === "string") {
+    const match = input.match(/^([0-9.]+) IOV$/);
+    if (!match) throw new Error("Got unexpected amount format");
+    return Decimal.fromUserInput(match[1], 9);
+  } else {
+    const whole = input.whole || 0;
+    const fractional = input.fractional || 0;
+    return Decimal.fromUserInput(`${whole}.${fractional.toFixed(0).padStart(9, "0")}`, 9);
+  }
+}
+
+async function main(): Promise<void> {
+  const doc = await getFile();
+
+  const wallets = doc.app_state.cash;
+  console.log("Wallets:", wallets.length);
+  const escrows = doc.app_state.escrow;
+  console.log("Escrows:", escrows.length);
+
+  const coinsFromWallets = wallets.map(wallet => {
+    if (wallet.coins.length !== 1) throw new Error("Unexpected number of coins in wallet");
+    return parseIov(wallet.coins[0]);
+  })
+
+  const coinsFromEscrows = escrows.map(escrow => {
+    if (escrow.amount.length !== 1) throw new Error("Unexpected number of amounts in escrow");
+    return parseIov(escrow.amount[0]);
+  })
+
+  const allCoins = [...coinsFromWallets, ...coinsFromEscrows];
+
+  const total = allCoins.reduce((current, added) => {
+    return add(current, added)
+  }, Decimal.fromAtomics("0", 9));
+  console.log("Total supply", total.toString());
+
+  for (const wallet of wallets) {
+    if (wallet.coins.length !== 1) throw new Error("Unexpected number of coins in wallet");
+    // console.log(wallet.coins[0]);
+    const iovCoins = parseIov(wallet.coins[0]);
+    const part = relative(iovCoins, total);
+    console.log(wallet.address, iovCoins.toString(), part);
+  }
+}
+
+main().then(
+  () => process.exit(0),
+  error => {
+    console.error(error);
+    process.exit(1);
+  }
+);
---
+++ src/math.ts
@@ -0,0 +1,18 @@
+import { Decimal } from "@iov/encoding"
+
+export function add(a: Decimal, b: Decimal): Decimal {
+  const abi = BigInt(a.atomics);
+  const bbi = BigInt(b.atomics);
+  return Decimal.fromAtomics((abi+bbi).toString(10), 9);
+}
+
+export function numericApproximation(decimal: Decimal) {
+  const out = Number(decimal.toString());
+  if (Number.isNaN(out)) throw new Error("Conversion to number failed");
+  return out;
+}
+
+export function relative(nominator: Decimal, denominator: Decimal): string {
+  const rel = numericApproximation(nominator) / numericApproximation(denominator);
+  return `${(rel*100).toFixed(2)} %`
+}
