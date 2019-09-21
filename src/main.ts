0d060b23069720ea964398dae8ef0a4cae8afcb2
Simon Warta <simon@warta.it>
2019-09-21 09:30:59 +0200

    Add linter support

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,39 @@
-const fs = require('fs');
-import {Decimal} from "@iov/encoding"
-import {add, relative} from "./math"
+/* eslint-disable no-console */
+import { Decimal } from "@iov/encoding";
+import fs from "fs";
 
-type Amount = string | { whole?: number, fractional?: number, ticker: string }
+import { add, relative } from "./math";
+
+type Amount = string | { whole?: number; fractional?: number; ticker: string };
 
 interface Wallet {
-  readonly address: string,
-  readonly coins: readonly Amount[],
+  readonly address: string;
+  readonly coins: readonly Amount[];
 }
 
 interface Escrow {
-  readonly "//name": string,
-  readonly arbiter: string,
-  readonly destination: string,
-  readonly source: string,
-  readonly timeout: string,
-  readonly amount: readonly Amount[],
+  readonly "//name": string;
+  readonly arbiter: string;
+  readonly destination: string;
+  readonly source: string;
+  readonly timeout: string;
+  readonly amount: readonly Amount[];
 }
 
 interface Genesis {
   readonly app_state: {
-    readonly cash: readonly Wallet[],
-    readonly escrow: readonly Escrow[],
-  }
+    readonly cash: readonly Wallet[];
+    readonly escrow: readonly Escrow[];
+  };
 }
 
 async function getFile(path: string): Promise<Genesis> {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, function (err: any, data: any) {
+    fs.readFile(path, function(err: any, data: any) {
       if (err) reject(err);
       else resolve(JSON.parse(data.toString()));
     });
-  })
+  });
 }
 
 function parseIov(input: Amount): Decimal {
@@ -57,20 +59,19 @@ async function main(args: readonly string[]): Promise<void> {
   const coinsFromWallets = wallets.map(wallet => {
     if (wallet.coins.length !== 1) throw new Error("Unexpected number of coins in wallet");
     return parseIov(wallet.coins[0]);
-  })
+  });
 
   const coinsFromEscrows = escrows.map(escrow => {
     if (escrow.amount.length !== 1) throw new Error("Unexpected number of amounts in escrow");
     return parseIov(escrow.amount[0]);
-  })
+  });
 
   const allCoins = [...coinsFromWallets, ...coinsFromEscrows];
 
   const total = allCoins.reduce((current, added) => {
-    return add(current, added)
+    return add(current, added);
   }, Decimal.fromAtomics("0", 9));
   console.log("Total supply", total.toString());
-
 
   for (const wallet of wallets) {
     if (wallet.coins.length !== 1) throw new Error("Unexpected number of coins in wallet");
@@ -94,5 +95,5 @@ main(process.argv.slice(2)).then(
   error => {
     console.error(error);
     process.exit(1);
-  }
+  },
 );
--- src/math.ts
+++ src/math.ts
@@ -1,12 +1,12 @@
-import { Decimal } from "@iov/encoding"
+import { Decimal } from "@iov/encoding";
 
 export function add(a: Decimal, b: Decimal): Decimal {
   const abi = BigInt(a.atomics);
   const bbi = BigInt(b.atomics);
-  return Decimal.fromAtomics((abi+bbi).toString(10), 9);
+  return Decimal.fromAtomics((abi + bbi).toString(10), 9);
 }
 
-export function numericApproximation(decimal: Decimal) {
+export function numericApproximation(decimal: Decimal): number {
   const out = Number(decimal.toString());
   if (Number.isNaN(out)) throw new Error("Conversion to number failed");
   return out;
@@ -14,5 +14,5 @@ export function numericApproximation(decimal: Decimal) {
 
 export function relative(nominator: Decimal, denominator: Decimal): string {
   const rel = numericApproximation(nominator) / numericApproximation(denominator);
-  return `${(rel*100).toFixed(3)}%`
+  return `${(rel * 100).toFixed(3)}%`;
 }
